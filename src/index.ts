b62477b2ff5f24b36ebe27fe2411db0e811df75b
shaon22 <shaonroy11@gmail.com>
2025-07-20 03:13:53 +0600

    set up basic express server using typscript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+app.use(express.json());
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from Express with TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
